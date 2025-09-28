f303ac59c0b29d9b7dc79cd51e6bd5412b2995c6
sharperi24 <sharanyaperi20@gmail.com>
2025-09-29 04:08:05 +0530

    final

--- server/server.py
+++ server/server.py
@@ -22,18 +22,20 @@ def get_location_names():
 
 @app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
-    data = request.get_json(force=True)
-    total_sqft = float(data['total_sqft'])
-    location = data['location']
-    bhk = int(data['bhk'])
-    bath = int(data['bath'])
+    try:
+        data = request.get_json(force=True)
+        total_sqft = float(data['total_sqft'])
+        location = data['location']
+        bhk = int(data['bhk'])
+        bath = int(data['bath'])
 
-    response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
-    })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
-    return response
+        estimated_price = util.get_estimated_price(location, total_sqft, bhk, bath)
+        response = jsonify({'estimated_price': estimated_price})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+    except Exception as e:
+        print("Error in /predict_home_price:", e)
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
